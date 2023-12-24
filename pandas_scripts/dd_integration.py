e8dceeb7c7c1b3a7e8b88592f847335e98c7c7e3
Nokodoko <cMonty614@gmail.com>
2023-12-23 23:34:23 -0500

    updated script with minimal formatting

--- pandas_scripts/dd_integration.py
+++ pandas_scripts/dd_integration.py
@@ -5,7 +5,6 @@ import requests
 import pandas as pd
 
 # TODO:
-# dataframe to_excel
 # Is there a dynamic way to include growing list of applications?
 
 api_key = os.getenv("DD_API_KEY")
@@ -16,22 +15,22 @@ azure = "azure"
 account_ids = {
     "007911250085": "FEMS",
     "066399073050": "CIAO",
-    # "107895115286": "LLC",
-    # "112364832438": "UAVS",
-    # "114245151275": "WFWEB",
-    # "171226545153": "NRMBP",
-    # "175560568060": "HWP",
-    # "227101269271": "HDWI",
-    # "328762180115": "CFTEAM",
-    # "403938590038": "GPH",
-    # "407464523575": "WFSAFE",
-    # "504420026195": "INCIWEB",
-    # "515022543707": "FSAPPSE",
-    # "583937734902": "VDR",
-    # "744281823657": "PAT",
-    # "748257752168": "NMIS-GrowHub",
-    # "878733051466": "Admin",
-    # "899201081254": "AFF",
+    "107895115286": "LLC",
+    "112364832438": "UAVS",
+    "114245151275": "WFWEB",
+    "171226545153": "NRMBP",
+    "175560568060": "HWP",
+    "227101269271": "HDWI",
+    "328762180115": "CFTEAM",
+    "403938590038": "GPH",
+    "407464523575": "WFSAFE",
+    "504420026195": "INCIWEB",
+    "515022543707": "FSAPPSE",
+    "583937734902": "VDR",
+    "744281823657": "PAT",
+    "748257752168": "NMIS-GrowHub",
+    "878733051466": "Admin",
+    "899201081254": "AFF",
 }
 
 headers = {
@@ -46,7 +45,6 @@ def integrations(app_id, app_name):
     url = link
     response = requests.get(url, headers=headers)
     if response.status_code != 200:
-        # TODO: add name to error and no just id
         print(f"{response.status_code} for {app_name}")
         return pd.DataFrame()
     else:
@@ -65,22 +63,25 @@ def integration_writer(list, cloud_service):
             df['account_id'] = account_id
             integration_list = pd.concat(
                 [integration_list, df], ignore_index=True)
-            integration_list.columns = [column.upper()
-                                        for column in integration_list.columns]
-            with pd.ExcelWriter('integrations_list.xlsx', engine='xlsxwriter') as writer:
-                integration_list.to_excel(
-                    writer, sheet_name=f'{cloud_service}_list', index=False)
-                workbook = writer.book
-                worksheet = writer.sheets[f'{cloud_service}_list']
+    integration_list.columns = [column.upper()
+                                for column in integration_list.columns]
+    with pd.ExcelWriter('integrations_list.xlsx', engine='xlsxwriter') as writer:
+        integration_list.to_excel(
+            writer, sheet_name=f'{cloud_service}_list', index=False)
+        workbook = writer.book
+        worksheet = writer.sheets[f'{cloud_service}_list']
 
-                for i, column in enumerate(integration_list.columns):
-                    column_len = integration_list[column].astype(
-                        str).apply(len)
-                    max_len = max(column_len.max(), len(column)) + 2
-                    worksheet.set_column(i, i, max_len)
+        for i, column in enumerate(integration_list.columns):
+            column_len = integration_list[column].astype(
+                str).apply(len)
+            max_len = max(column_len.max(), len(column)) + 2
+            worksheet.set_column(i, i, max_len)
 
-            # integration_list.to_excel(
-            #    f'{cloud_service}_list.xlsx', index=False)
+        account_name_index = integration_list.columns.get_loc(
+            'ACCOUNT_NAME')
+
+    worksheet.autofilter(0, account_name_index, len(
+        integration_list), account_name_index)
 
 
 integration_writer(account_ids, aws)
